ddd2c57453124ae38c8e01cca47b77c5dc1ba624
Naprimerov <naprimerov2016@yandex.ru>
2019-11-03 23:23:37 +0300

    Сделал задание

--- js/main.js
+++ js/main.js
@@ -1,69 +1,65 @@
-var avatarArray = ['img/avatars/user01', 'img/avatars/user02', 'img/avatars/user03', 'img/avatars/user04', 'img/avatars/user05', 'img/avatars/user06', 'img/avatars/user07', 'img/avatars/user08'];
+var avatarArray = ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png'];
 var typeArray = ['palace', 'flat', 'house', 'bungalo'];
 var checkinArray = ['12:00', '13:00', '14:00'];
 var checkoutArray = ['12:00', '13:00', '14:00']
 var featuresArray = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var photosArray = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var propertyArray = [];
-var minY = 130;
-var maxY = 630;
-var random = Math.floor(Math.random() * (maxY - minY + 1)) + minY;
-featuresArray.length = Math.round(Math.random()* 6 + 1);
+var pinMinY = 130;
+var pinMaxY = 630;
+var pinMinX = 0;
+var pinMaxX = 1200;
+var pinHeight = 70;
+var pinWidth = 50;
+var getRandomInt = function (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
 
 var arrayObject = function (property) {
-  for (var i = 1; i < 8; i++) {
-   var property = {
+  for (var i = 0; i < 8; i++) {
+    var property = {
       'author': {
-        'avatar': avatarArray[Math.round(Math.random() * 8 + 1)]
+        'avatar': avatarArray[Math.floor(Math.random() * avatarArray.length)]
       },
-
       'offer': {
         'title': 'Объявление',
         'address': "600, 350",
         'price': 50000,
-        'type': typeArray[Math.round(Math.random() * 4 + 1)],
+        'type': typeArray[Math.floor(Math.random() * typeArray.length)],
         'rooms': 3,
         'guests': 3,
-        'checkin': checkinArray[Math.round(Math.random() * 3 + 1)],
-        'checkout': checkoutArray[Math.round(Math.random() * 3 + 1)],
-        'features': featuresArray,
+        'checkin': checkinArray[Math.floor(Math.random() * checkinArray.length)],
+        'checkout': checkoutArray[Math.floor(Math.random() * checkoutArray.length)],
+        'features': featuresArray[Math.floor(Math.random() * featuresArray.length)],
         'description': 'строка с описанием',
-        'photos': photosArray[Math.round(Math.random() * 3 + 1)]
+        'photos': photosArray[Math.floor(Math.random() * photosArray.length)]
       },
-
       'location': {
-        'x': 80,
-        'y': random
+        'x': getRandomInt(pinMinX + pinWidth, pinMaxX - pinWidth),
+        'y': getRandomInt(pinMinY + pinHeight, pinMaxY - pinHeight)
       }
     };
-  } if (i < 8) {
-    propertyArray[i] = property;
+    propertyArray.push(property);
   };
 };
-
-var maps = document.querySelector('map--faded');
+var maps = document.querySelector('.map');
 maps.classList.remove('map--faded');
+arrayObject();
 
-var renderItem = function (pinElement) {
-  var pins = document.querySelectorAll('#pin');
+var renderItem = function (pinData) {
   var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
-
-  for (var i = 1; i > propertyArray.length; i++) {
   var pinElement = pinTemplate.cloneNode(true);
+  pinElement.querySelector('img').setAttribute('src', pinData.author.avatar);
+  pinElement.querySelector('img').setAttribute('alt', pinData.author.avatar);
+  pinElement.setAttribute('style', 'left: ' + pinData.location.x + 'px; top: ' + pinData.location.y + 'px;');
 
-  pinElement.querySelector('.map__pin').setAttribute('src', 'img/avatars/user07.png');
-  pinElement.querySelector('.map__pin').setAttribute('alt', 'Метка объявления');
-  pinElement.querySelector('.map__pin').setAttribute('style', 'left: 200px; top: 400px;');
-
-  };
+  return pinElement;
 };
 
 var creatonDomElements = function () {
-  pins = document.querySelectorAll('#pin');
-  pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
-  for (var i = 0; i < 8; i++) {
-    var marcOnCard = pins.cloneNode(true);
-    marcOnCard.children[0].textContent = i;
-    propertyArray[1].appendChild(marcOnCard);
+  var pinContainer = document.querySelector('.map__pins');
+  for (var i = 0; i < propertyArray.length; i++) {
+    pinContainer.appendChild(renderItem(propertyArray[i]));
   };
 };
+creatonDomElements();
